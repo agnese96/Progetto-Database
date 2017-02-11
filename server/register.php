c9e19cf9e2eaf7b238237a88ce9e6b148a852530
EnricoPuccia <enrico.puccia@gmail.com>
2017-02-11 20:11:12 +0100

    Registrazione lato server

--- server/register.php
+++ server/register.php
@@ -1,4 +1,16 @@
 <?php
+require "connection.php";
+$cognome = $_POST["cognome"];
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$password = password_hash( $_POST["password"]);
 
+$stmt = $conn->prepare("INSERT INTO Utenti(nome, cognome, email, password) VALUES (?,?,?,?)");
+$stmt->bind_param("ssss", $nome, $cognome, $email, $password);
 
+if($stmt->execute()) {
+  $data = (object) ['success' => true];
+}
+else $data = (object) ['error' => 'Non so cosa sia successo :D'];
+echo json_encode($data);
 ?>
