0c2885e7e3255134d7cbc959556d18d3165936a1
EnricoPuccia <enrico.puccia@gmail.com>
2017-02-17 17:17:24 +0100

    Controllo se il token è stato compromesso

--- server/login.php
+++ server/login.php
@@ -1,9 +1,11 @@
 <?php
 
   require './lib/JWT.php';
-  
+
   $token = array();
   $token['id'] = "agnese@mail.it";
   echo JWT::encode($token, 'secret_server_key');
 
+  $token  = JWT::decode($_POST['token'], 'secret_server_key');
+  echo $token->id;
  ?>
