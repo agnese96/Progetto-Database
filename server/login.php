de076dd434e2f2c36f0e9938c0e14853562efbd2
EnricoPuccia <enrico.puccia@gmail.com>
2017-02-17 18:13:08 +0100

    Pagine Login, implementazione

--- server/login.php
+++ server/login.php
@@ -2,10 +2,32 @@
 
   require './lib/JWT.php';
 
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+/*  if(strlen ($username) == 0) || strlen($password) == 0) {
+    header("Location: loginView.html");
+    exit;
+  }*/
+
+  $connessione=mysql_connect("localhost", "root", "");
+	if(mysql_select_db(Agenda))
+	{
+		$sql="SELECT * FROM Utenti WHERE email='$username' AND password='$password';";
+		$ris1 = mysql_query($sql);
+		$ris = mysql_fetch_array($ris1);
+		if($ris){
+			//$_SESSION['un'] = $username;
+			header("Location: home.php");
+			exit;
+		} else
+			echo "Nome utente o password errati. Riprova!";
+	}
+	else mysql_error ();
+	mysql_close($connessione);
+
   $token = array();
   $token['id'] = "agnese@mail.it";
   echo JWT::encode($token, 'secret_server_key');
 
-  $token  = JWT::decode($_POST['token'], 'secret_server_key');
-  echo $token->id;
  ?>
