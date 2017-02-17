8dc5bd525439c9c61f4a5cb5b807f047d2efd35b
Agnese Folli <agnesefolli@gmail.com>
2017-02-17 17:20:47 +0100

    Revert "Controllo se il token è stato compromesso"
    This reverts commit 0c2885e7e3255134d7cbc959556d18d3165936a1.

--- server/login.php
+++ server/login.php
@@ -1,11 +1,9 @@
 <?php
 
   require './lib/JWT.php';
-
+  
   $token = array();
   $token['id'] = "agnese@mail.it";
   echo JWT::encode($token, 'secret_server_key');
 
-  $token  = JWT::decode($_POST['token'], 'secret_server_key');
-  echo $token->id;
  ?>
