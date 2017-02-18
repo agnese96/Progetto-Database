a652fb1efd77ae47d86fd1eea45d68d25bacc3bc
EnricoPuccia <enrico.puccia@gmail.com>
2017-02-18 21:10:36 +0100

    FIX CreateEventView.html AND CreateEvent.php
    In CreateEventView.html ho descritto meglio i campi.
    In CreateEvent.php ho aggiunto dei cicil per poter inserire più volte
    gli eventi che si ripetono

--- Server/CreateEvent.php
+++ Server/CreateEvent.php
@@ -7,20 +7,43 @@ $Frequenza = $conn->real_escape_string($_POST["Frequenza"]);
 $Promemoria = $conn->real_escape_string($_POST["Promemoria"]);
 $DataInizio = $conn->real_escape_string($_POST["DataInizio"]);
 $OraInizio = $conn->real_escape_string($_POST["OraInizio"]);
-$DataInizio .= $OraInizio;
+//$DataInizio .= $OraInizio;
 $DataFine = $conn->real_escape_string($_POST["DataFine"]);
 $OraFine = $conn->real_escape_string($_POST["OraFine"]);
-$DataFine .= $OraFine;
+//$DataFine .= $OraFine;
 
-$stmt = $conn->prepare("INSERT INTO DateEvento(DataInizio, DataFine) VALUES(?,?)");
-$stmt->bind_param("ss", $DataInizio, $DataFine);
+for($x = 0; $x < $Ricorrenza; $x++)   /* if user inserted some value */
+{
+  $stmt = $conn->prepare("INSERT INTO DateEvento(DataInizio, DataFine) VALUES(?,?)");
+  $DataI = $DataInizio.$OraInizio;    //unisco gg-mm-aaaa con l'orario
+  $DataF = $DataFine.$OraFine;    //unisco gg-mm-aaaa con l'orario
+  $stmt->bind_param("ss", $DataI, $DataF);
+  $DataInizio = date('Y-m-d', strtotime($DataInizio."+ $Frequenza days"));    //incremento la data inizio di un numero di giorni pari a "frequenza"
+  $DataFine = date('Y-m-d', strtotime($DataFine."+ $Frequenza days"));    //incremento la data fine di un numero di giorni pari a "frequenza"
 
-$stmt1 = $conn->prepare("INSERT INTO Eventi(Titolo, Descrizione, Ricorrenza, Frequenza, Promemoria, NomeCategoria) VALUES(?,?,?,?,?,?)");
-$stmt1->bind_param("ssiiis", $Titolo,$Descrizione,$Ricorrenza,$Frequenza,$Promemoria,$Promemoria);
-
-if($stmt->execute() && $stmt1->execute()) {
-  $data = (object) ['success' => true];
+  $stmt1 = $conn->prepare("INSERT INTO Eventi(Titolo, Descrizione, Ricorrenza, Frequenza, Promemoria, NomeCategoria) VALUES(?,?,?,?,?,?)");
+  $stmt1->bind_param("ssiiis", $Titolo,$Descrizione,$Ricorrenza,$Frequenza,$Promemoria,$Promemoria);
 }
-else $data = (object) ['error' => 'Si è verificato un errore :('];
+
+if($Ricorrenza == 0)  /* if user didn't insert any value */
+  for($x = 0; $x < 24; $x++)
+  {
+    $stmt = $conn->prepare("INSERT INTO DateEvento(DataInizio, DataFine) VALUES(?,?)");
+    $DataI = $DataInizio.$OraInizio;    //unisco gg-mm-aaaa con l'orario
+    $DataF = $DataFine.$OraFine;    //unisco gg-mm-aaaa con l'orario
+    $stmt->bind_param("ss", $DataI, $DataF);
+    $DataInizio = date('Y-m-d', strtotime($DataInizio."+ $Frequenza days"));    //incremento la data inizio di un numero di giorni pari a "frequenza"
+    $DataFine = date('Y-m-d', strtotime($DataFine."+ $Frequenza days"));    //incremento la data fine di un numero di giorni pari a "frequenza"
+
+    $stmt1 = $conn->prepare("INSERT INTO Eventi(Titolo, Descrizione, Ricorrenza, Frequenza, Promemoria, NomeCategoria) VALUES(?,?,?,?,?,?)");
+    $stmt1->bind_param("ssiiis", $Titolo,$Descrizione,$Ricorrenza,$Frequenza,$Promemoria,$Promemoria);
+  }
+
+if($stmt->execute() && $stmt1->execute())
+  $data = (object) ['success' => true];
+else
+  $data = (object) ['error' => 'Si è verificato un errore :('];
+  
 echo json_encode($data);
+
 ?>
