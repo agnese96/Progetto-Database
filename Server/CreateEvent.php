44c4bd9f70ad5e304e370c2f237153b01eff7c4b
EnricoPuccia <enrico.puccia@gmail.com>
2017-02-18 20:19:53 +0100

    FIX CreateEvent.php
    Aggiunta seconda query, ora manca solo il token

--- Server/CreateEvent.php
+++ Server/CreateEvent.php
@@ -3,7 +3,6 @@ require "connection.php"
 $Titolo = $conn->real_escape_string($_POST["Titolo"]);
 $Descrizione = $conn->real_escape_string($_POST["Descrizione"]);
 $Ricorrenza = $conn->real_escape_string($_POST["Ricorrenza"]);
-$Ricorrenza = $conn->real_escape_string($_POST["Ricorrenza"]);
 $Frequenza = $conn->real_escape_string($_POST["Frequenza"]);
 $Promemoria = $conn->real_escape_string($_POST["Promemoria"]);
 $DataInizio = $conn->real_escape_string($_POST["DataInizio"]);
@@ -13,12 +12,15 @@ $DataFine = $conn->real_escape_string($_POST["DataFine"]);
 $OraFine = $conn->real_escape_string($_POST["OraFine"]);
 $DataFine .= $OraFine;
 
-$stmt = $conn->prepare("INSERT INTO DateEvento(DataInizio, DataFine) VALUES(?,?)")
+$stmt = $conn->prepare("INSERT INTO DateEvento(DataInizio, DataFine) VALUES(?,?)");
 $stmt->bind_param("ss", $DataInizio, $DataFine);
 
-if($stmt->execute()) {
+$stmt1 = $conn->prepare("INSERT INTO Eventi(Titolo, Descrizione, Ricorrenza, Frequenza, Promemoria, NomeCategoria) VALUES(?,?,?,?,?,?)");
+$stmt1->bind_param("ssiiis", $Titolo,$Descrizione,$Ricorrenza,$Frequenza,$Promemoria,$Promemoria);
+
+if($stmt->execute() && $stmt1->execute()) {
   $data = (object) ['success' => true];
 }
-else $data = (object) ['error' => 'Si è verificato un errore :()'];
+else $data = (object) ['error' => 'Si è verificato un errore :('];
 echo json_encode($data);
 ?>
