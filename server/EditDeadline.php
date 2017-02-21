3555af8fe62a8de3737ca5b0f1d3b61dafadb5e0
Agnese Folli <agnesefolli@gmail.com>
2017-02-21 20:30:38 +0100

    FIXED EDIT ADDED COMPLETE

---
+++ server/EditDeadline.php
@@ -0,0 +1,26 @@
+<?php
+  require 'connection.php';
+  $IDUtente = require 'lib/decodeToken.php';
+  $IDScadenza = $conn->real_escape_string($_POST['IDScadenza']);
+  if(! checkOwnerDeadline($IDUtente, $IDScadenza, $conn)) {
+    echo json_encode($data=['error' => $conn->error]);
+    exit();
+  }
+
+  $Descrizione = $conn->real_escape_string($_POST['Descrizione']);
+  $Data = $conn->real_escape_string($_POST['Data']);
+  $Promemoria = $conn->real_escape_string($_POST['Promemoria']);
+  $Frequenza = $conn->real_escape_string($_POST['Frequenza']);
+  $Ricorrenza = $conn->real_escape_string($_POST['Ricorrenza']);
+
+  $sql = "UPDATE Scadenze
+          SET Descrizione='$Descrizione', Data='$Data', Promemoria=$Promemoria, Frequenza=$Frequenza, Ricorrenza=$Ricorrenza
+          WHERE IDScadenza='$IDScadenza'";
+  if(! $result = $conn->query($sql)) {
+    echo json_encode($data = ['error' => $conn->error]);
+    exit();
+  }
+
+  echo json_encode($data = ['success' => true, 'idscadenza' => $IDScadenza]);
+  $conn->close();
+ ?>
