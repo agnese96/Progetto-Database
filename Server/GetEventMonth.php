193541852b01621e568e74c8ce7dcc448f64b5e1
EnricoPuccia <enrico.puccia@gmail.com>
2017-03-01 20:54:51 +0100

    ANOTHER FIX on GetEventDay/Month/Week.php

--- Server/GetEventMonth.php
+++ Server/GetEventMonth.php
@@ -4,9 +4,17 @@ $IDUtente = require "lib/decodeToken.php";
 $Month = $conn->real_escape_string($_POST['Month']);
 $Year = $conn->real_escape_string($_POST['Year']);
 
+/*$sql = "SELECT e.IDEvento, Titolo, de.DataInizio, OraInizio, DataFine, OraFine, NomeCategoria
+        FROM (DateEvento de JOIN Eventi e ON e.IDEvento = de.IDEvento) JOIN Invitare i ON (e.IDEvento=i.IDEvento AND de.DataInizio=i.DataInizio)
+        WHERE month(de.DataInizio) = $Month AND year(de.DataInizio) = $Year AND (e.IDCreatore='$IDUtente' OR i.Email='$IDUtente')";*/
+
 $sql = "SELECT e.IDEvento, Titolo, de.DataInizio, OraInizio, DataFine, OraFine, NomeCategoria
         FROM (DateEvento de JOIN Eventi e ON e.IDEvento = de.IDEvento) JOIN Invitare i ON (e.IDEvento=i.IDEvento AND de.DataInizio=i.DataInizio)
-        WHERE month(de.DataInizio) = $Month AND year(de.DataInizio) = $Year AND (e.IDCreatore='$IDUtente' OR i.Email='$IDUtente')";
+        WHERE (month(de.DataInizio) <= $Month OR month(de.DataFine) >= $Month) AND (year(de.DataInizio) <= $Year OR year(de.DataFine) >= $Year) AND i.Email='$IDUtente'
+        UNION
+        SELECT e.IDEvento, Titolo, de.DataInizio, OraInizio, DataFine, OraFine, NomeCategoria
+        FROM DateEvento de JOIN Eventi e ON e.IDEvento = de.IDEvento
+        WHERE (month(de.DataInizio) <= $Month OR month(de.DataFine) >= $Month) AND (year(de.DataInizio) <= $Year OR year(de.DataFine) >= $Year) AND e.IDCreatore='$IDUtente'";
 
 if(! $result = $conn->query($sql)) {
   echo json_encode($data = ['error' => $conn->error]);
--- Server/GetEventWeek.php
+++ Server/GetEventWeek.php
@@ -4,9 +4,17 @@ $IDUtente = require "lib/decodeToken.php";
 $Week = $conn->real_escape_string($_POST['Week']);
 $Year = $conn->real_escape_string($_POST['Year']);
 
+/*$sql = "SELECT e.IDEvento, Titolo, de.DataInizio, OraInizio, DataFine, OraFine, NomeCategoria
+        FROM (DateEvento de JOIN Eventi e ON e.IDEvento = de.IDEvento) JOIN Invitare i ON (e.IDEvento=i.IDEvento AND de.DataInizio=i.DataInizio)
+        WHERE week(de.DataInizio) = $Week AND year(de.DataInizio) = $Year AND (e.IDCreatore='$IDUtente' OR i.Email='$IDUtente')";*/
 $sql = "SELECT e.IDEvento, Titolo, de.DataInizio, OraInizio, DataFine, OraFine, NomeCategoria
         FROM (DateEvento de JOIN Eventi e ON e.IDEvento = de.IDEvento) JOIN Invitare i ON (e.IDEvento=i.IDEvento AND de.DataInizio=i.DataInizio)
-        WHERE week(de.DataInizio) = $Week AND year(de.DataInizio) = $Year AND (e.IDCreatore='$IDUtente' OR i.Email='$IDUtente')";
+        WHERE (week(de.DataInizio) <= $Week OR week(de.DataFine) >= $Week) AND (year(de.DataInizio) <= $Year OR year(de.DataFine) >= $Year) AND i.Email='$IDUtente'
+        UNION
+        SELECT e.IDEvento, Titolo, de.DataInizio, OraInizio, DataFine, OraFine, NomeCategoria
+        FROM DateEvento de JOIN Eventi e ON e.IDEvento = de.IDEvento
+        WHERE (week(de.DataInizio) <= $Week OR week(de.DataFine) >= $Week) AND (year(de.DataInizio) <= $Year OR year(de.DataFine) >= $Year) AND e.IDCreatore='$IDUtente'";
+
 
 if(! $result = $conn->query($sql)) {
   echo json_encode($data = ['error' => $conn->error]);
