49aa05bb53d0c896d4f81edb8a2d18501cff161b
Agnese Folli <agnesefolli@gmail.com>
2017-03-01 18:35:02 +0100

    Non mi ricordo

--- App/Services/UserService.js
+++ App/Services/UserService.js
@@ -5,6 +5,7 @@ class UserService {
     this.$window=$window;
     this.$rootScope=$rootScope;
 
+    this.name=this.$window.localStorage.getItem('name');
     this.photo=this.$window.localStorage.getItem('photo');
     this.email=this.$window.localStorage.getItem('email');
     this.token=this.$window.localStorage.getItem('token');
@@ -15,10 +16,11 @@ class UserService {
     console.log(this.token);
   }
 
-  updateLocalStorage(token, email, photo) {
+  updateLocalStorage(token, email, photo, name) {
     this.$window.localStorage.setItem('token', token);
     this.$window.localStorage.setItem('email', email);
     this.$window.localStorage.setItem('photo', photo);
+    this.$window.localStorage.setItem('name', name);
     //this.$http.defaults.headers.common['Authentication'] = 'Bearer: ' + token;
     this.$rootScope.$broadcast('userChange');//emit an event so that the other controllers can update their info.
   }
@@ -28,7 +30,8 @@ class UserService {
     this.logged=true;
     this.photo=res.photo;
     this.token=res.token;
-    this.updateLocalStorage(res.token, res.email, res.photo);
+    this.name=res.name;
+    this.updateLocalStorage(res.token, res.email, res.photo, this.name);
   }
 
   login(credentials, callback){
@@ -58,7 +61,7 @@ class UserService {
   }
 
   isLogged() { return this.logged; }
-  gUser()    { return { email: this.email, photo: this.photo }; }
+  gUser()    { return { email: this.email, photo: this.photo, name:this.name }; }
   gMail()    { return this.email; }
   gToken()   { return this.token; }
 }
--- server/login.php
+++ server/login.php
@@ -4,14 +4,14 @@
   require 'connection.php';
   $email = $conn->real_escape_string($_POST['email']);
   $password = $_POST['password'];
-  
+
 /*$conn->real_escape_string() corregge eventuali errori dovuti ad apici ecc ecc */
 /*  if(strlen ($email) == 0) || strlen($password) == 0) {
     header("Location: loginView.html");
     exit;
   }*/
 
-	$sql="SELECT FotoProfilo, Password FROM Utenti WHERE email='$email';";
+	$sql="SELECT FotoProfilo, Password, CONCAT_WS(' ', Cognome, Nome) AS Nominativo FROM Utenti WHERE email='$email';";
   $ris = $conn->query($sql);
 	if($ris->num_rows == 1){
     $row = $ris->fetch_assoc();//come fetch array
@@ -19,7 +19,7 @@
       $data = array();
       $data['email'] = $email;
       $token = JWT::encode($data, 'secret_server_key');
-      $data = (object) ['token' => $token, 'email' => $email, 'photo' => $row['FotoProfilo']];
+      $data = (object) ['token' => $token, 'email' => $email, 'photo' => $row['FotoProfilo'], 'name' => $row['Nominativo']];
       echo json_encode($data);
       exit;
     } else
